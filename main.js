0fc65a049a30fb68f7f3bccf9ade00ea3be3f965
Delfina Cañas <79326402+delfigica@users.noreply.github.com>
2025-09-17 11:04:34 -0300

    update information

--- main.js
+++ main.js
@@ -26,19 +26,33 @@ const experience = [
 
 const estudios = [
   {
+    lugar: "Univesidad Siglo XXI",
+    periodo: "2025-now",
+    descripción: "Bachelor's degree in artificial intelligence and robotics",
+  },
+  {
     lugar: "Nueva Escuela Juan Mantovani",
     periodo: "2011-2018",
     descripción: "Bachelor in natural sciences",
   },
 ];
 
+const skills = [{
+  group: 'Frontend',
+  skills: 'HTML, CSS, JavaScript, TypeScript, React, Next.js, Material UI, Vue, Nuxt, Nuxt UI'
+},
+{
+  group: 'Tools and others',
+  skills: 'Git/GitHub, Strapi, SEO, Responsive Web Designs'
+}]
+
 // Change the name of the H1 that has the user's name
-var nombreDOM = document.getElementById("name");
+let nombreDOM = document.getElementById("name");
 nombreDOM.innerText = nombre;
 
 // Itineration that creates the items in "EXPERIENCE"
 for (i = 0; i < experience.length; i++) {
-  var item = document.createElement("div");
+  let item = document.createElement("div");
   item.innerHTML = `<span class="exp-position">${experience[i].position}</span>
   <div class="d-flex align-items-center">
     <span class="exp-title mr-2">${experience[i].place} </span> 
@@ -52,7 +66,7 @@ for (i = 0; i < experience.length; i++) {
 
 // Itineration that creates the items in "EDUCATION & CERTIFICATIONS"
 for (i = 0; i < estudios.length; i++) {
-  var item = document.createElement("div");
+  let item = document.createElement("div");
   item.innerHTML = `<div class="d-flex justify-content-between">
     <span class="card-title h6 exp-title">${estudios[i].lugar}</span>
     <span class="exp-date">${estudios[i].periodo}</span>
@@ -61,9 +75,18 @@ for (i = 0; i < estudios.length; i++) {
   document.getElementById("estudios").appendChild(item);
 }
 
+// Itineration that creates the items in "technical skills TECHNICAL SKILLS"
+for(i= 0; i < skills.length; i++) {
+  let item = document.createElement("div");
+  item.innerHTML = `<div>
+  <p><b class="text-uppercase">${skills[i].group}</b>: ${skills[i].skills}</p>
+  </div>`;
+  document.getElementById("skills").appendChild(item)
+}
+
 function printDiv(printable) {
-  var printContents = document.getElementById(printable).innerHTML;
-  var originalContents = document.body.innerHTML;
+  let printContents = document.getElementById(printable).innerHTML;
+  let originalContents = document.body.innerHTML;
 
   document.body.innerHTML = printContents;
 
